de5c3ebcba168d1269003d42fe2b942fcd84ba37
RAMRATAN SHARMA <77084946+nittratan@users.noreply.github.com>
2025-08-17 17:25:07 +0530

    Update Myfile1.py

--- Myfile1.py
+++ Myfile1.py
@@ -1,8 +1,1 @@
-Hi Team,
 
-We use AWS instances for PostgreSQL data storage and GCP for application deployments. We have redeployed from our side, and currently all services are running fine. CPU utilization and overall system health are normal at this point.
-
-We will continue to monitor the services.
-
-Thanks,
-Ramratan
