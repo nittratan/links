ff4bb928c820a0906dd10ec72f304a9b832ca9ff
RAMRATAN SHARMA <77084946+nittratan@users.noreply.github.com>
2025-08-24 10:49:03 +0530

    Create query.py

---
+++ query.py
@@ -0,0 +1,21 @@
+if filter_obj:
+    for key, value in filter_obj.items():
+        if value is None:  # handle IS NULL
+            stmt = stmt.where(getattr(table.c, key).is_(None))
+        elif isinstance(value, list):
+            if value:  # skip empty list
+                stmt = stmt.where(getattr(table.c, key).in_(value))
+        else:
+            stmt = stmt.where(getattr(table.c, key) == value)
+
+
+
+if neg_filter_obj:
+    for key, value in neg_filter_obj.items():
+        if value is None:  # handle IS NOT NULL
+            stmt = stmt.where(getattr(table.c, key).is_not(None))
+        elif isinstance(value, list):
+            if value:  # skip empty list
+                stmt = stmt.where(getattr(table.c, key).notin_(value))
+        else:
+            stmt = stmt.where(getattr(table.c, key) != value)
